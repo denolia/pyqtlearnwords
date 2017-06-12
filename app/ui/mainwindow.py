1fb27f9d1880d395e4e795f0f49187d5f48c81d9
julia vikulina <julia.vikulina@gmail.com>
2017-06-12 16:34:10 +0300

    table is filled with dummy data

--- app/ui/mainwindow.py
+++ app/ui/mainwindow.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 
 from app.ui.mainwindow_ui import Ui_MainWindow
 from app.ui.manager import Manager
+from app.ui.table.tabledata import TableData
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -14,7 +15,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
-    manager = Manager(mainwindow)
+    table_data = TableData()
+    manager = Manager(mainwindow, table_data)
     manager.setup()
     mainwindow.show()
     app.exec_()
--- app/ui/manager.py
+++ app/ui/manager.py
@@ -1,16 +1,27 @@
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTableWidgetItem
 
 
 class Manager:
-    def __init__(self, mainwindow: 'MainWindow'):
+    def __init__(self, mainwindow: 'MainWindow', table: 'TableData'):
         self.mainwindow = mainwindow
+        self.table = table
 
     def setup(self):
-
         self.mainwindow.tableWidget.clear()
         self.updateTable()
 
     def updateTable(self):
-        table = []
-        self.mainwindow.tableWidget.setRowCount(2)
-        self.mainwindow.tableWidget.setColumnCount(5)
+        table = self.table.get_data()
+        self.mainwindow.tableWidget.setRowCount(len(table))
+        self.mainwindow.tableWidget.setColumnCount(len(table))
+        self.mainwindow.tableWidget.setHorizontalHeaderLabels(["Title", "Year", "Mins", "Acquired", "Notes"])
+
+        for n_row, row in enumerate(table):
+            for n_column, val in enumerate(row):
+                item = QTableWidgetItem(str(val))
+                item.setData(Qt.ToolTipRole, 'hi there')
+                item.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+                self.mainwindow.tableWidget.setItem(n_row, n_column, item)
+
+        self.mainwindow.tableWidget.resizeColumnsToContents()
